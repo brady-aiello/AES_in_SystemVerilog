243deb567644c11f6642d5c81517d72e96e29166
Brady <brady.aiello@gmail.com>
2017-12-01 20:30:31 -0800

    "Made shift_rows 1 dimensional. Added mix_columns skeleton."

---
+++ AES_1.srcs/sources_1/new/mix_columns.sv
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/01/2017 08:27:21 PM
+// Design Name: 
+// Module Name: mix_columns
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mix_columns(
+    input [127:0] in,
+    output [127:0] out
+    );
+endmodule
--- AES_1.srcs/sources_1/new/shift_rows.sv
+++ AES_1.srcs/sources_1/new/shift_rows.sv
@@ -21,17 +21,34 @@
 
 
 module shift_rows(
-    input  [7:0] in [3:0][3:0],
-    output [7:0] out [3:0][3:0]
+    input  logic  in [127:0],
+    output logic out [127:0]
     );
-    assign out[0]= in[0];
     
-    assign out[1][0:2] = in[1][1:3];
-    assign out[1][3] = in[1][1];
+    assign out[127:96]= in[127:96];
     
-    assign out[2][0:1] = in[2][2:3];
-    assign out[2][2:3] = in[2][0:1];
+    assign out[95:72] = in[87:64];
+    assign out[71:64] = in[95:88];
     
-    assign out[3][0] = in[3][3];
-    assign out[3][1:3] = in[3][0:2];
+    assign out[63:48] = in[47:32];
+    assign out[47:32] = in[63:48];
+    
+    assign out[31:24] = in[7:0];
+    assign out[23:0] = in[31:24];
 endmodule
+
+//module shift_rows(
+//    input  [7:0] in [3:0][3:0],
+//    output [7:0] out [3:0][3:0]
+//    );
+//    assign out[0]= in[0];
+    
+//    assign out[1][0:2] = in[1][1:3];
+//    assign out[1][3] = in[1][1];
+    
+//    assign out[2][0:1] = in[2][2:3];
+//    assign out[2][2:3] = in[2][0:1];
+    
+//    assign out[3][0] = in[3][3];
+//    assign out[3][1:3] = in[3][0:2];
+//endmodule
