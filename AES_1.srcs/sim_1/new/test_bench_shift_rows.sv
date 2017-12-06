da45a866a6f6010b06825ae92fa3273fca3ade1f
Brady <brady.aiello@gmail.com>
2017-12-06 11:52:58 -0800

    Fixed file structure.
    Signed-off-by: Brady <brady.aiello@gmail.com>

--- AES_1.srcs/sim_1/new/test_bench_shift_rows.sv
+++ AES_1.srcs/sim_1/new/test_bench_shift_rows.sv
@@ -23,11 +23,12 @@
 module test_bench_shift_rows();
     logic  [127:0] in_t1;
     wire   [127:0] out_t1;
-    
+     
     shift_rows shift_rows0(in_t1, out_t1);
     
     initial begin
-         in_t1 <= 'h01020304010203040102030401020304;
+         //in_t1 <= 'h01020304010203040102030401020304;
+         in_t1 <=   'h000102030405060708090A0B0C0D0E0F;
          #1;
 //         in_t1 <= 'h00010203000102030001020300010203;
 //         #1;
